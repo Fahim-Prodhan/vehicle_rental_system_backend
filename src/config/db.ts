f91a1753fe75e94cd2a2df5d91a1cae479c8654c
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2026-02-09 02:21:36 +0600

    Users database configed

--- src/config/db.ts
+++ src/config/db.ts
@@ -18,6 +18,18 @@ const initDB = async () => {
     );
 `);
 
+  await pool.query(`
+            CREATE TABLE IF NOT EXIST vehicles
+            id SERIAL PRIMARY KEY,
+            vehicle_name VARCHAR(250) NOT NULL,
+            type VARCHAR(10) CHECK(type IN('car', 'bike', 'van', 'SUV')),
+            registration_number NOT NULL UNIQUE,
+            daily_rent_price NOT NULL CHECK(daily_rent_price > 0),
+            daily_rent_price CHECK(daily_rent_price IN('available', 'booked'))
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+
+        `);
 };
 
 export default initDB;
